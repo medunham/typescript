e2b4f44a264a03b4f41770fc4c158e74a9ce08bf
michael.dunham <michael.dunham@amwell.com>
2022-05-19 23:58:19 -0700

    feat(gilded-rose): add Conjured item behavior
    note: follow existing code pattern without refactor

--- katas/gilded-rose/src/gilded-rose.ts
+++ katas/gilded-rose/src/gilded-rose.ts
@@ -42,15 +42,24 @@ export class GildedRose {
           }
         }
       }
+
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+
+      if (this.items[i].name == 'Conjured') {
+        this.items[i].quality = this.items[i].quality - 1;
+      }
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].quality = this.items[i].quality - 1;
+                if (this.items[i].name == 'Conjured') {
+                  this.items[i].quality = this.items[i].quality - 1;
+                }
               }
             }
           } else {
