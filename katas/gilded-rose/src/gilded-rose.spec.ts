6942c7a85836515b0c5f398269d5cc757b8e49aa
michael.dunham <michael.dunham@amwell.com>
2022-05-19 23:40:19 -0700

    chore(gilded-rose): add test coverage based on existing requirements
    see [GildedRose-Refactoring-Kata/GildedRoseRequirements.txt](https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/main/GildedRoseRequirements.txt) for more details

--- katas/gilded-rose/src/gilded-rose.spec.ts
+++ katas/gilded-rose/src/gilded-rose.spec.ts
@@ -1,9 +1,250 @@
 import { Item, GildedRose } from './gilded-rose';
 
+// const pass = new Item('Backstage passes to a TAFKAL80ETC concert', 1, 1);
+// const mace = new Item('Sulfuras, Hand of Ragnaros', 5, 5);
+// const other = new Item('Other', 20, 20);
+
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+  let items: Item[];
+
+  beforeEach(() => {
+    items = [new Item('example', 10, 10)];
+  });
+
+  describe('when constructing', () => {
+    it('defaults the items property', () => {
+      const job = new GildedRose();
+      expect(job.items).toEqual([]);
+    });
+
+    it('sets the items property', () => {
+      const job = new GildedRose(items);
+      expect(job.items).toEqual(items);
+    });
+  });
+
+  describe('when updating', () => {
+    let job: GildedRose;
+    let item: Item;
+
+    beforeEach(() => {
+      job = new GildedRose();
+    });
+
+    describe('a backstage pass', () => {
+      describe('with more than 10 days until the sellIn date', () => {
+        beforeEach(() => {
+          item = new Item('Backstage passes to a TAFKAL80ETC concert', 11, 10);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('decreases the sellIn by 1', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn - 1);
+        });
+
+        it('increases the quality by 1', () => {
+          expect(job.items[0].quality).toEqual(item.quality + 1);
+        });
+      });
+
+      describe('with between 5 and 10 days until the sellIn date', () => {
+        beforeEach(() => {
+          item = new Item('Backstage passes to a TAFKAL80ETC concert', 9, 10);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('decreases the sellIn by 1', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn - 1);
+        });
+
+        it('increases the quality by 2', () => {
+          expect(job.items[0].quality).toEqual(item.quality + 2);
+        });
+      });
+
+      describe('with between 1 and 5 days left until the sellIn date', () => {
+        beforeEach(() => {
+          item = new Item('Backstage passes to a TAFKAL80ETC concert', 4, 10);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('decreases the sellIn by 1', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn - 1);
+        });
+
+        it('increases the quality by 3', () => {
+          expect(job.items[0].quality).toEqual(item.quality + 3);
+        });
+      });
+
+      describe('is past the sellIn date', () => {
+        beforeEach(() => {
+          item = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('decreases the sellIn by 1', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn - 1);
+        });
+
+        it('sets the quality to zero', () => {
+          expect(job.items[0].quality).toEqual(0);
+        });
+      });
+
+      describe('and the quality is 50', () => {
+        beforeEach(() => {
+          item = new Item('Backstage passes to a TAFKAL80ETC concert', 4, 50);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('decreases the sellIn by 1', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn - 1);
+        });
+
+        it('does not increase the quality any futher', () => {
+          expect(job.items[0].quality).toEqual(50);
+        });
+      });
+    });
+
+    describe('aged brie', () => {
+      describe('and the sellIn date is in the future', () => {
+        beforeEach(() => {
+          item = new Item('Aged Brie', 4, 20);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('decreases the sellIn by 1', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn - 1);
+        });
+
+        it('increases the quality by 1', () => {
+          expect(job.items[0].quality).toEqual(item.quality + 1);
+        });
+      });
+
+      describe('and the sellIn date has passed', () => {
+        beforeEach(() => {
+          item = new Item('Aged Brie', 0, 20);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('decreases the sellIn by 1', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn - 1);
+        });
+
+        it('increases the quality by 2', () => {
+          expect(job.items[0].quality).toEqual(item.quality + 2);
+        });
+      });
+
+      describe('and the quality is 50', () => {
+        beforeEach(() => {
+          item = new Item('Aged Brie', 10, 50);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('decreases the sellIn by 1', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn - 1);
+        });
+
+        it('does not increase the quality any futher', () => {
+          expect(job.items[0].quality).toEqual(50);
+        });
+      });
+    });
+
+    describe('sulfuras, hand of ragnaros', () => {
+      describe('and the sellIn date is in the future', () => {
+        beforeEach(() => {
+          item = new Item('Sulfuras, Hand of Ragnaros', 4, 80);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('sellIn does not change', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn);
+        });
+
+        it('quality does not change', () => {
+          expect(job.items[0].quality).toEqual(item.quality);
+        });
+      });
+
+      describe('and the sellIn date has passed', () => {
+        beforeEach(() => {
+          item = new Item('Sulfuras, Hand of Ragnaros', -1, 80);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('sellIn does not change', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn);
+        });
+
+        it('quality does not change', () => {
+          expect(job.items[0].quality).toEqual(item.quality);
+        });
+      });
+    });
+
+    describe('other items', () => {
+      describe('and the sellIn date is in the future', () => {
+        beforeEach(() => {
+          item = new Item('other', 4, 20);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('decreases the sellIn by 1', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn - 1);
+        });
+
+        it('decreases the quality by 1', () => {
+          expect(job.items[0].quality).toEqual(item.quality - 1);
+        });
+      });
+
+      describe('and the sellIn date has passed', () => {
+        beforeEach(() => {
+          item = new Item('other', -1, 20);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('decreases the sellIn by 1', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn - 1);
+        });
+
+        it('decreases the quality by 2', () => {
+          expect(job.items[0].quality).toEqual(item.quality - 2);
+        });
+      });
+
+      describe('and the quality is 0', () => {
+        beforeEach(() => {
+          item = new Item('other', 4, 0);
+          job = new GildedRose([{ ...item }]);
+          job.updateQuality();
+        });
+
+        it('decreases the sellIn by 1', () => {
+          expect(job.items[0].sellIn).toEqual(item.sellIn - 1);
+        });
+
+        it('quality does not change', () => {
+          expect(job.items[0].quality).toEqual(item.quality);
+        });
+      });
+    });
   });
 });
