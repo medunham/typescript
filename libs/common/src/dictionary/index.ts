173a8c9f186ff3387c895e12c1f8d69dfe2fb883
michael.dunham <michael.dunham@amwell.com>
2022-05-20 01:08:37 -0700

    feat(common): string and number dictionary definitions
    create common interfaces for hash objects with a string or number key

---
+++ libs/common/src/dictionary/index.ts
@@ -0,0 +1,2 @@
+export * from './number-dictionary';
+export * from './string-dictionary';
--- libs/common/src/index.spec.ts
+++
@@ -1,8 +0,0 @@
-// Note: this file is just a module smoke test
-import { HELLO_WORLD } from './index';
-
-describe('module', () => {
-  it('exports constants', () => {
-    expect(HELLO_WORLD).toEqual('Hello World');
-  });
-});
