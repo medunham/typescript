e53c25ac8fc1eceaaee058406dfcd428b8f247d7
Michael Dunham <medunham@Michaels-MacBook-Pro-2.local>
2022-06-14 12:28:37 -0700

    chore(proxy): add route for default angular app

--- services/proxy/src/index.ts
+++ services/proxy/src/index.ts
@@ -1,8 +1,9 @@
 import * as express from 'express';
 
-import { createProxyMiddleware } from 'http-proxy-middleware'; 
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 
 app.use('/api/auth', createProxyMiddleware({ target: 'http://localhost:4100', pathRewrite: { '^/api/auth': '' } }));
+app.use('', createProxyMiddleware({ target: 'http://localhost:4200' }))
 app.listen(4000);
